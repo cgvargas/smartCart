7ad1c6363bb5ea7d77f16b9af2a998aede80c7f4
Claudio <claudiog.vargas@outlook.com>
2026-01-09 06:49:15 -0300

    chore: Remove unused TokenObtainPairSerializer import

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -3,7 +3,6 @@ SmartCart Accounts Serializers
 """
 
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 
